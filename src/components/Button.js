15508672b6ba1eccee396f59e630b323259a2ab6
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2021-02-07 05:49:35 -0500

    reusable button component with props

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text }) => {
+    const onClick = () => {
+        console.log('click')
+    }
+    return <button onclick={onClick} style={{ backgroundColor: color }} className='btn'>{text}</button>
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
+}
+export default Button
