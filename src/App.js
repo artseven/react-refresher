2a419bb2717a7ce0f667fc17a47ff03f342b452b
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2021-02-15 09:06:28 -0500

    passing method down as prop

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'Metting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
+      id: 2,
+      text: "Metting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-  ])
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+  //Delete task
+  const deleteTask = (id) => {
+    console.log("delete", id);
+  };
   return (
     <div className="container">
-      <Header title="Task Tracker" />
-      <Tasks tasks={tasks} />
+      <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
-}
+};
 
 export default App;
