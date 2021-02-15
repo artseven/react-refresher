b6beade16690c0eef259d5d99810e8e3eb3a4617
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2021-02-15 09:34:49 -0500

    conditional rendering

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,10 +4,10 @@ const Task = ({ task, onDelete }) => {
   return (
     <div className="task">
       <h3>
-        {task.text}{" "}
+        {task.text}
         <FaTimes
           style={{ color: "red", cursor: "pointer" }}
-          onClick={onDelete}
+          onClick={() => onDelete(task.id)}
         />
       </h3>
       <p>{task.day}</p>
