74dc51e279cb9104e802eebb54760eb8a67dec94
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2021-02-15 10:18:17 -0500

    dynamic class assignment

--- src/App.js
+++ src/App.js
@@ -23,15 +23,25 @@ const App = () => {
       reminder: false,
     },
   ]);
+
   //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show"
       )}
